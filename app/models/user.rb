bdcabde7602593aca09c99b3786174f90620627a
Alan Krajina <alankrajina88@gmail.com>
2019-11-07 15:28:10 +0100

    added serializer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    belongs_to :hometown, class_name: "Location"
+
 
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,39 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :username, :password_digest
+  attributes :id, :name, :username, :hometown
 end
+
+
+
+
+
+
+
+
+=begin shift=alt=a
+
+class UserSerializer < ActiveModel::Serializer
+  attributes :id, :name, :username, :hometown
+end
+
+http://localhost:3000/api/v1/users
+
+[
+  {
+  id: 1,
+  name: "Nadia",
+  username: "nadiaf",
+  hometown: {
+      id: 1,
+      city: "London",
+      state: "London",
+      country: "UK",
+      created_at: "2019-11-07T14:10:51.419Z",
+      updated_at: "2019-11-07T14:10:51.419Z"
+  }
+  }
+  ] 
+  
+  GEM>
+   gem 'active_model_serializers'
+
+  =end
