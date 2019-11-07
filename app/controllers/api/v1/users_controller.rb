967564e6218d0bea63e80df2952833d4695e8cd0
Alan Krajina <alankrajina88@gmail.com>
2019-11-07 14:41:32 +0100

    added api/v1 routes and folder

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
   # GET /users
@@ -25,18 +25,18 @@ class UsersController < ApplicationController
   end
 
   # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
+#  def update
+#    if @user.update(user_params)
+#      render json: @user
+#    else
+#      render json: @user.errors, status: :unprocessable_entity
+#    end
+#  end
 
   # DELETE /users/1
-  def destroy
-    @user.destroy
-  end
+#  def destroy
+#    @user.destroy
+#  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
